76a5ae81f5c96e17610bac5b9e6eec8560e1dee5
Keegan Street <keeganstreet@gmail.com>
2011-07-11 21:25:30 +0200

    added analytics and meta tags for deployment

--- public/js/client.js
+++ public/js/client.js
@@ -84,3 +84,11 @@ var velo = (function(module) {
 
 }(velo || {}));
 
+var _gaq = [['_setAccount', 'UA-9800583-13'], ['_trackPageview']];
+(function(d, t) {
+var g = d.createElement(t),
+    s = d.getElementsByTagName(t)[0];
+g.async = g.src = '//www.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g, s);
+}(document, 'script'));
+
